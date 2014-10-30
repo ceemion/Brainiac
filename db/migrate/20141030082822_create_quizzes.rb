ff3637f4384d803de039fcfdba81579c33e7130b
ceemion <fiyinsimeon@gmail.com>
2014-10-30 09:44:55 +0100

    created table columns and ran 'rake db:migrate'

--- db/migrate/20141030082822_create_quizzes.rb
+++ db/migrate/20141030082822_create_quizzes.rb
@@ -1,12 +1,21 @@
 class CreateQuizzes < ActiveRecord::Migration
-  def change
+  def up
     create_table :quizzes do |t|
       t.string :question
-      t.column "option" :array
+      t.column "option", :array
       t.string :answer
       t.string :category
 
       t.timestamps
+    end
+
+    def down
+    	t.timestamps
+    	t.string :category
+    	t.string :answer
+    	t.column "option", :array
+    	t.string :question
+    drop_table("quizzes")
     end
   end
 end
